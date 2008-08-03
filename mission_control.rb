7b364f429ac3d57772016891a69c3c541e8148ef
collin <collintmiller@gmail.com>
2008-08-03 15:08:06 -0500

    typo

--- mission_control.rb
+++ mission_control.rb
@@ -5,12 +5,12 @@ Legs.start do
   def say(message, cookie)
     current_user = User.find(cookie.split("=").last.unpack('*m').first[:user_id])
     broadcast :said, current_user.name, message
-    reutrn "ok"
+    "ok"
   end
 
   def next_track(data)
-    broadcast :play, Song.first.url
-    return "ok"
+    broadcast :play, Song.all.last.url
+    "ok"
   end
 end
 
